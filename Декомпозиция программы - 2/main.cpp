214151b1a8081c269474a3d309e31591505f6680
VodopshinV <84332962+VodopshinV@users.noreply.github.com>
2022-01-27 22:18:43 +0200

    Add files via upload

---
+++ Декомпозиция программы - 2/main.cpp
@@ -0,0 +1,9 @@
+#include"bus_manager.h"
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
---
+++ Декомпозиция программы - 2/query.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<sstream>
+#include<vector>
+#include<string>
+using namespace std;
+
+enum class QueryType {
+    NewBus,
+    BusesForStop,
+    StopsForBus,
+    AllBuses
+};
+
+struct Query {
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
+};
+
+istream& operator >> (istream& is, Query& q);
